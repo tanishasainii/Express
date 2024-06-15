881916c5ff216d3018fb1c760429a87a87e4c752
tanishasainii <sainitanisha967@gmail.com>
2024-06-15 19:23:51 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'Express')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,  'index.html'));
+});
+
+
+app.listen(port, ()=>{
+  console.log('server is running on '+ port);
+});
